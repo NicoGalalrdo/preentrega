ef718e36b10386896a1ee485f025685e8c719b0a
Nicolas Gallardo <gallardonicolas463@gmail.com>
2026-07-30 10:09:20 -0300

    Pre-entrega-2

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
-let mensaje;
+{mensaje = "Bienvenido a canjes ropa_indumentarias010"
+alert (mensaje)}
 
-let nombre = prompt("ingresa tu nombre");
-mensaje = "bienvenido " + nombre;
-console.log(mensaje);
-alert(mensaje);
+let contrasena = ""
+let usuario = ""
+while (usuario !== "Nico123" || contrasena !== "Nico2005"){
 
-let edad = parseInt(prompt("ingresa tu edad"));
-mensaje = "tu edad es : " + edad;
-console.log(mensaje);
-alert(mensaje);
+    usuario = prompt("Ingresa tu usuario")
+    contrasena = prompt ("ingresa tu contrasena")
 
-let resultado = 2026 - edad;
-mensaje = nombre + ", naciste en " + resultado;
-console.log(mensaje);
-alert(mensaje);
+    if(usuario !== "Nico123" || contrasena !== "Nico2005"){
+    console.log("Usuario o contraseña incorrecta")
+    
+    mensaje = "Contraseña incorrecta, vuelve a intentar"
+    alert (mensaje)}
+}
+console.log("Bienvenido")
+mensaje = "Bienvenido"
+alert (mensaje)
 
-let Localidad = prompt("¿De qué país eres?");
-mensaje = "Perfecto, te recomendaremos sobre " + Localidad;
-console.log(mensaje);
-alert(mensaje);
+let cuenta = (prompt("Inegresa el nombre de tu perfil"))
+    console.log("Nombre de la cuenta : " + cuenta)
+mensaje = "El nombre de tu cuenta se registro como " + cuenta;
+alert (mensaje)
+let seguidores = Number(prompt("Ingresa tu numero de seguidores"))
+if (seguidores >= 10000){
+    console.log("Numero de seguidores : " + seguidores )
+mensaje = "Numero de seguidores suficientes para un canje"
+alert (mensaje)
+}else{
+    console.log("No tienes suficientes seguidores para un canje")
+mensaje = "No tienes suficientes seguidores para un canje"
+alert (mensaje)
+}
+
+
