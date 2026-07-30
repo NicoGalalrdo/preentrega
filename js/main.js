9d944bfaeba64fb2fcf6713f5c6d10ec2a06e80b
Nicolas Gallardo <gallardonicolas463@gmail.com>
2026-07-30 07:53:15 -0300

    preentrega 1

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let mensaje;
+
+let nombre = prompt("ingresa tu nombre");
+mensaje = "bienvenido " + nombre;
+console.log(mensaje);
+alert(mensaje);
+
+let edad = parseInt(prompt("ingresa tu edad"));
+mensaje = "tu edad es : " + edad;
+console.log(mensaje);
+alert(mensaje);
+
+let resultado = 2026 - edad;
+mensaje = nombre + ", naciste en " + resultado;
+console.log(mensaje);
+alert(mensaje);
+
+let Localidad = prompt("¿De qué país eres?");
+mensaje = "Perfecto, te recomendaremos sobre " + Localidad;
+console.log(mensaje);
+alert(mensaje);
