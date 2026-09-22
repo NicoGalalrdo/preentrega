e5054a9c5f95376802b430a45da53aac3b77fcb9
Nicolas Gallardo <gallardonicolas463@gmail.com>
2026-09-22 11:31:17 -0300

    preentrega 7

--- js/main.js
+++ js/main.js
@@ -1,47 +1,124 @@
-function inicio () {
-    let name = prompt("Ingresa tu nombre")
-    alert ("Hola " + name + " Bienvenido a nuestra tienda de ropa")
+const productos = [
+  {
+    id: 1,
+    nombre: "Pantalon",
+    descripcion: "Pantalon de algodón para hombre",
+    precio: "$15000",
+    imagen: "👖",
+  },
+  {
+    id: 2,
+    nombre: "remera",
+    descripcion: "Remera unisex",
+    precio: "$8000",
+    imagen: "👕",
+  },
+  {
+    id: 3,
+    nombre: "medias",
+    descripcion: "Medias de algodón para hombre",
+    precio: "$5000",
+    imagen: "🧦",
+  },
+  {
+    id: 4,
+    nombre: "zapatillas",
+    descripcion: "Zapatillas de moda",
+    precio: "$60000",
+    imagen: "👟",
+  },
+  {
+    id: 5,
+    nombre: "zapatos",
+    descripcion: "Zapatos de vestir para mujer",
+    precio: "$35000",
+    imagen: "👠",
+  },
+
+];
+
+
+const carrito = [];
+
+    const titulo = document.getElementById("titulo");
+console.log(titulo);
+titulo.innerText = "Mi emprendimiento";
+
+    const texto = document.createElement("p");
+texto.innerText = "Explora nuestra tienda";
+console.log(texto);
+
+    const subtitulo = document.querySelector("h2");
+subtitulo.appendChild(texto);
+
+
+function imprimirProductos() {
+
+const contenedorProductos = document.getElementById("productos");
+contenedorProductos.innerHTML = "";
+
+productos.forEach((producto) => {
+
+const card = document.createElement("article");
+
+card.classList.add("card");
+card.innerHTML += `
+    
+
+      <span>${producto.imagen}</span>
+      <h3>${producto.nombre}</h3>
+      <p>${producto.descripcion}</p>
+      <p>${producto.precio}</p>
+        <button id="${producto.id}">Agregar al carrito</button>
+
+  `;
+
+    contenedorProductos.appendChild(card);
+
+  const btncarrito = document.getElementById(`${producto.id}`);
+  btncarrito.addEventListener("click", () => {
+    Toastify({
+    text: `Producto agregado al carrito: ${producto.nombre}`,
+    duration: 3000,
+    gravity: "top", 
+    position: "center", 
+    onClick: function(){} 
+}).showToast();
+    
+    carrito.push(producto);
+
+  }) 
+});
 }
 
-inicio()
-
-const prendas = ["camisa", "pantalón", "zapatos", "gorra", "bufanda"]
-console.log(prendas.join(", "))
-console.log("tenemos un total de: " + prendas.length + " prendas")
-
-prendas.push ("cinturon")
-console.log(prendas.join(", "))
-console.log("Se agrego una prenda al final del catologo")
-console.log("tenemos un total de: " + prendas.length + " prendas")
+imprimirProductos();
 
 
-prendas.unshift("chaleco")
-console.log(prendas.join(", "))
-console.log("Se agrego una prenda al inicio del catalogo")
-console.log("tenemos un total de: " + prendas.length + " prendas")
+function agregarProducto () {
+    const formParaProductos = document.getElementById("form-agregar-producto")
+    formParaProductos.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-const eliminada = prendas.pop()
-console.log(prendas.join(", "))
-console.log("se elimino la prenda: " + eliminada )
-console.log("tenemos un total de: " + prendas.length + " prendas")
+    const inputNombre = document.getElementById("input-nombre").value;
+   
 
-console.log("Se reemplazo la prenda de indice 1 por shorts")
-prendas.splice(1, 1, "shorts")
-console.log(prendas.join(", "))
+    const inputPrecio = document.getElementById("input-precio").value;
+    
+    
+    const inputImagen = document.getElementById("input-imagen").value;
+    
+
+    const inputDescripcion = document.getElementById("input-descripcion").value;
+    
 
 
-let buscar = prompt("Ingrese la prenda que desea buscar: ")
-if(prendas.includes(buscar)){
-    console.log("Se encuentra en nuestro catalogo como prenda con indice: " + prendas.indexOf(buscar))
-    alert ("Se encuentra en nuestro catalogo como prenda con indice: " + prendas.indexOf(buscar)) 
-}
-else{
-    console.log("No se encuentra en nuestro catalogo el producto")
-    alert ("No se encuentra en nuestro catalogo el producto")
+    productos.push({nombre: inputNombre, precio: inputPrecio, imagen: inputImagen,
+        descripcion: inputDescripcion
+    })
+    
+
+    imprimirProductos()
+});
 }
 
-
-
-for(const prenda of prendas){
-    console.log("prenda con indice " + prendas.indexOf(prenda) + ": " + prenda)
-}
+agregarProducto();
