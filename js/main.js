901ed9b6f53e7844d59a4be3eb20100bd76f2b6e
Nicolas Gallardo <gallardonicolas463@gmail.com>
2026-09-24 01:03:26 -0300

    Nueva preentrega basada en Preentrega 7

--- js/main.js
+++ js/main.js
@@ -1,138 +1,124 @@
-const productosIniciales = [
+const productos = [
   {
     id: 1,
-    producto: "Lechuga",
+    nombre: "Pantalon",
+    descripcion: "Pantalon de algodón para hombre",
+    precio: "$15000",
+    imagen: "👖",
   },
   {
     id: 2,
-    producto: "Tomate",
+    nombre: "remera",
+    descripcion: "Remera unisex",
+    precio: "$8000",
+    imagen: "👕",
   },
   {
     id: 3,
-    producto: "Pera",
+    nombre: "medias",
+    descripcion: "Medias de algodón para hombre",
+    precio: "$5000",
+    imagen: "🧦",
   },
   {
     id: 4,
-    producto: "Banana",
+    nombre: "zapatillas",
+    descripcion: "Zapatillas de moda",
+    precio: "$60000",
+    imagen: "👟",
   },
   {
     id: 5,
-    producto: "Manzana",
+    nombre: "zapatos",
+    descripcion: "Zapatos de vestir para mujer",
+    precio: "$35000",
+    imagen: "👠",
   },
+
 ];
 
 
-
-if (!localStorage.getItem("misProductos")) {
-  localStorage.setItem("misProductos", JSON.stringify(productosIniciales));
-}
-
-
-
-let productos = JSON.parse(localStorage.getItem("misProductos"));
-
 const carrito = [];
+
+    const titulo = document.getElementById("titulo");
+console.log(titulo);
+titulo.innerText = "Mi emprendimiento";
+
+    const texto = document.createElement("p");
+texto.innerText = "Explora nuestra tienda";
+console.log(texto);
+
+    const subtitulo = document.querySelector("h2");
+subtitulo.appendChild(texto);
 
 
 function imprimirProductos() {
 
-  const contenedorProductos = document.getElementById("productos");
+const contenedorProductos = document.getElementById("productos");
+contenedorProductos.innerHTML = "";
 
-  contenedorProductos.innerHTML = "";
+productos.forEach((producto) => {
 
-  productos.forEach((producto) => {
+const card = document.createElement("article");
 
-    const card = document.createElement("article");
+card.classList.add("card");
+card.innerHTML += `
+    
 
-    card.classList.add("card");
+      <span>${producto.imagen}</span>
+      <h3>${producto.nombre}</h3>
+      <p>${producto.descripcion}</p>
+      <p>${producto.precio}</p>
+        <button id="${producto.id}">Agregar al carrito</button>
 
-    card.innerHTML = `
-      <span>🛒</span>
-      <h3>${producto.producto}</h3>
-      <button id="${producto.id}">Agregar al carrito</button>
-    `;
+  `;
 
     contenedorProductos.appendChild(card);
 
-    const btncarrito = document.getElementById(`${producto.id}`);
-
-    btncarrito.addEventListener("click", () => {
-
-      Toastify({
-        text: `Producto agregado al carrito: ${producto.producto}`,
-        duration: 3000,
-        gravity: "top",
-        position: "center",
-        onClick: function () {}
-      }).showToast();
-
-      carrito.push(producto);
-    });
-
-  });
-}
-
-
-
-const inputProducto = document.getElementById("inputProducto");
-const btnAgregar = document.getElementById("btnAgregar");
-const btnEliminar = document.getElementById("btnEliminar");
-const btnRestaurar = document.getElementById("btnRestaurar")
-
-function mostrarEnDOM(){ imprimirProductos() }
-
-
-btnAgregar.addEventListener("click", () => {
-
-  const texto = inputProducto.value.trim();
-
-  if (texto !== "") {
-
-    const nuevoProducto = {
-      id: Date.now(),
-      producto: texto
-    };
-
-    productos.push(nuevoProducto);
-
-  
-    localStorage.setItem(
-      "misProductos",
-      JSON.stringify(productos)
-    );
-
-    imprimirProductos();
-
+  const btncarrito = document.getElementById(`${producto.id}`);
+  btncarrito.addEventListener("click", () => {
+    Toastify({
+    text: `Producto agregado al carrito: ${producto.nombre}`,
+    duration: 3000,
+    gravity: "top", 
+    position: "center", 
+    onClick: function(){} 
+}).showToast();
     
-    inputProducto.value = "";
-  }
+    carrito.push(producto);
+
+  }) 
 });
-
-
+}
 
 imprimirProductos();
 
 
-btnEliminar.addEventListener("click", () => {
+function agregarProducto () {
+    const formParaProductos = document.getElementById("form-agregar-producto")
+    formParaProductos.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-  productos = [];
+    const inputNombre = document.getElementById("input-nombre").value;
+   
 
-  localStorage.setItem("misProductos", JSON.stringify(productos));
-  imprimirProductos();
+    const inputPrecio = document.getElementById("input-precio").value;
+    
+    
+    const inputImagen = document.getElementById("input-imagen").value;
+    
 
+    const inputDescripcion = document.getElementById("input-descripcion").value;
+    
+
+
+    productos.push({nombre: inputNombre, precio: inputPrecio, imagen: inputImagen,
+        descripcion: inputDescripcion
+    })
+    
+
+    imprimirProductos()
 });
+}
 
-
-
-btnRestaurar.addEventListener("click", () => {
-
-  productos = productosIniciales;
-
-  localStorage.setItem(
-    "misProductos",
-    JSON.stringify(productos)
-  );
-
-  imprimirProductos();
-
-});
+agregarProducto();
